db2400b68a3d93c70902706871722226a9f9522b
sekitaka <sekitaka.tech@gmail.com>
2013-02-16 09:47:50 +0900

    First commit

---
+++ test/index.js
@@ -0,0 +1,16 @@
+
+function f1(){
+    var e = new Task() ;
+    setTimeout(function(){
+        e.done() ;
+    },1000)
+    return e;
+}
+function f2(){
+    var e = new Task() ;
+    setTimeout(function(){
+        e.done() ;
+    },3000)
+    return e;
+}
+
