50de555b759c6e84eb7a510648998d5e9e8a9964
sekitaka <sekitaka.tech@gmail.com>
2013-02-18 21:58:38 +0900

    Implement serial function.

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require('./lib/node-flow.js') ;
+module.exports.NodeFlow = require('./lib/node-flow.js') ;
+module.exports.Task = require('./lib/task.js') ;
--- test/index.js
+++ test/index.js
@@ -1,16 +1,52 @@
-// hoge
+var NF = require('../index.js').NodeFlow ;
+var Task = require('../index.js').Task ;
+
 function f1(){
+    console.log('f1 START');
     var e = new Task() ;
     setTimeout(function(){
+        console.log('f1 END');
         e.done() ;
     },1000)
     return e;
 }
 function f2(){
+    console.log('f2 START');
     var e = new Task() ;
     setTimeout(function(){
+        console.log('f2 END');
         e.done() ;
     },3000)
     return e;
 }
 
+function f3(){
+    console.log('f3 START');
+    var flgBreak = false ;
+    var st = new Date(),
+        et ;
+    while(true){
+        et = new Date() ;
+        if ( (et-st) > 2000 ) break ;
+    }
+    console.log('f3 END');
+}
+
+
+// parallel
+//NF.parallel([f3,f3],function(err,num){
+//    if (err) {
+//        console.log('ERROR') ;
+//    } else {
+//        console.log('ALL TASK DONE[' + num + ']') ;
+//    }
+//})
+
+// serial
+NF.serial([f2,f3,f1,f2,f3,f1],function(err){
+    if(err){
+        console.log('ERROR') ;
+    } else {
+        console.log('ALL TASK DONE.') ;
+    }
+})
